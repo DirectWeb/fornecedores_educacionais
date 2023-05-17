51cec84dda096325171abbc35f51fef37a7b0daa
Luan Gama <123767829+luangama@users.noreply.github.com>
2023-05-17 15:01:20 -0300

    Initial files

---
+++ app.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("botao-categorias");
+
+button.addEventListener("click", function() {
+
+    var conteiner = document.getElementById("menu-categorias");
+
+    conteiner.classList.toggle("hide");
+});
