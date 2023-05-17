458085d6d4f9415657d36cc803ddfb7d0e3a378e
Luan Gama <luanmsgama@gmail.com>
2023-05-17 16:25:39 -0300

    new page

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var button = document.getElementById("botao-categorias");
 button.addEventListener("click", function() {
 
     var conteiner = document.getElementById("menu-categorias");
-
-    conteiner.classList.toggle("hide");
+    if (conteiner.style.display === "block") {
+        conteiner.style.display = "none";
+    } else {
+        conteiner.style.display = "block";
+    }
 });
